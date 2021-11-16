07dfa269b4d4904c42f1cd39c6d35073cf8bd81d
aravind <aravind.r@optisolbusiness.com>
2021-11-16 18:20:08 +0530

    fix style folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
+import './assets/css/index.css'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
